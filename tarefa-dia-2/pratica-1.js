9d9eab91c99d8f61decb2fb62bb8d79491aee456
Rafael Albano <rafaelalbanooo@gmail.com>
2022-08-25 21:53:08 -0300

    usei o console.log para imprimir as informações contidas no arquivo pratica-1.js

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,10 +1,10 @@
-let nome
-nome = Rafael 
-let email
-email = rafaelalbanooogmail.com
-let profissao 
-profissao = estudante
-let idade 
-idade = 16 
-let lugar
-lugar = pantanal
+let nome = "rafael"
+let email = "rafaelalbanooogmail.com"
+let profissao = "estudante"
+let idade = 16
+let lugar = "pantanal"
+console.log(nome)
+console.log(email)
+console.log(profissao)
+console.log(idade)
+console.log(lugar)
